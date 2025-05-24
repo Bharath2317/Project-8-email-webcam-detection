641711bbf9334ed867072a13387b010a7f94a4a6
bharath <bharathh1817@gmail.com>
2025-05-24 11:21:42 +0530

    Added emailing process and Threading is done to prevent frame lag

--- emailing.py
+++ emailing.py
@@ -1,2 +1,35 @@
-def send_email():
-    print("Email Sent!")
+import smtplib
+from email.message import EmailMessage
+from PIL import Image
+import mimetypes
+
+PASSWORD = "hkzcqnnnbbxgsqgh"
+SENDER = "bharathh1817@gmail.com"
+RECEIVER = "bharathh1817@gmail.com"
+
+def send_email(image_path):
+    email_message = EmailMessage()
+    email_message["Subject"] = "You got a new customer!"
+    email_message.set_content("Hey! The customer has just arrived")
+
+    with open(image_path , "rb") as file:
+        content = file.read()
+
+    with Image.open(image_path) as img:
+        image_format = img.format.lower()
+    email_message.add_attachment(content,maintype = "image",subtype= image_format)
+
+    gmail = smtplib.SMTP("smtp.gmail.com",587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER,PASSWORD)
+    gmail.sendmail(SENDER,RECEIVER,email_message.as_string())
+    gmail.quit()
+
+
+if __name__ == "__main__":
+    send_email("images/255.png")
+
+
+    
+    
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import glob
-
 import cv2
 import time
+import os
 from emailing import send_email
+from threading import Thread
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
@@ -10,6 +11,12 @@ time.sleep(1)
 first_frame = None
 status_list = []
 count = 1
+
+def clean_folder():
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
+
 while True:
     status = 0
 
@@ -45,7 +52,14 @@ while True:
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        email_thread = Thread(target=send_email,args=(image_with_object, ))
+        email_thread.daemon = True
+        clean_thread = Thread(target=clean_folder)
+        email_thread.daemon = True
+
+        email_thread.start()
+
+
     print(status_list)
     cv2.imshow("Video",frame)
     key = cv2.waitKey(1)
@@ -54,3 +68,4 @@ while True:
         break
 
 video.release()
+clean_thread.start()
